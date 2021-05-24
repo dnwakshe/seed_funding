599aae496cd8b27a69585256883d930a9b6d449a
dnwakshe <[wakshe2@gmail.com]>
2021-05-24 12:05:34 +0530

    firebase is runing main page is functioning

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:seed_funding/Login.dart';
-import 'package:seed_funding/grid.dart';
 import 'package:seed_funding/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 
