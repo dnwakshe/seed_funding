31016bd54b400d1c39a47ceb238b15d6f07ae734
dnwakshe <[wakshe2@gmail.com]>
2021-05-25 11:39:42 +0530

    moduls Map added

---
+++ lib/moduls/user_data.dart
@@ -0,0 +1,12 @@
+class User_data {
+  String user_first_name;
+  String user_last_name;
+  String email;
+  User_data(this.user_first_name,this.user_last_name,this.email);
+    List<Map> myProducts = [
+
+    {"user_first_name": 'Dnyaneshwar', "user_last_name": "Wakshe","email":"wakshe2@gmail.com"},
+    
+    
+  ];
+}
